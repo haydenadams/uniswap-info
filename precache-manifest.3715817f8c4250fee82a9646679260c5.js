d9c0a70baa54c577d502399f59b3e6d0f23ca965
Conlan Rios <conlan@gmail.com>
2018-11-20 20:51:25 -0800

    Updates

---
+++ precache-manifest.3715817f8c4250fee82a9646679260c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ac065f8cde4ab8b14a8",
+    "url": "/uniswap-history/static/js/runtime~main.0ac065f8.js"
+  },
+  {
+    "revision": "b13706b386ee25f52097",
+    "url": "/uniswap-history/static/js/main.b13706b3.chunk.js"
+  },
+  {
+    "revision": "22c4f04dfee16f1d5c80",
+    "url": "/uniswap-history/static/js/1.22c4f04d.chunk.js"
+  },
+  {
+    "revision": "b13706b386ee25f52097",
+    "url": "/uniswap-history/static/css/main.72e44bcf.chunk.css"
+  },
+  {
+    "revision": "58b89d3fd90e2c774ef20e975f954e9f",
+    "url": "/uniswap-history/index.html"
+  }
+];
