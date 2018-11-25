427473de403e3e43be8b85b8052f0b24fb4a912f
Conlan Rios <conlan@gmail.com>
2018-11-25 03:15:01 -0800

    Updates

--- precache-manifest.3c099a47a3c2ca9082d0e40a84054f99.js
+++ precache-manifest.3c099a47a3c2ca9082d0e40a84054f99.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/uniswap-history/static/js/runtime~main.0ac065f8.js"
   },
   {
-    "revision": "bcfb5c44d893c25d7079",
-    "url": "/uniswap-history/static/js/main.bcfb5c44.chunk.js"
+    "revision": "2985c9870d33b3a42c78",
+    "url": "/uniswap-history/static/js/main.2985c987.chunk.js"
   },
   {
     "revision": "d463b3b69ac50a365d3f",
     "url": "/uniswap-history/static/js/1.d463b3b6.chunk.js"
   },
   {
-    "revision": "bcfb5c44d893c25d7079",
-    "url": "/uniswap-history/static/css/main.b15dc722.chunk.css"
+    "revision": "2985c9870d33b3a42c78",
+    "url": "/uniswap-history/static/css/main.0a57ee1d.chunk.css"
   },
   {
-    "revision": "28ff29249c44bd22084de35fc814fa1a",
+    "revision": "c573181d7f5050b4bfafd15008d914b7",
     "url": "/uniswap-history/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/uniswap-history/precache-manifest.1e3b61e96843d2cad870ec7f7ab7a98f.js"
+  "/uniswap-history/precache-manifest.3c099a47a3c2ca9082d0e40a84054f99.js"
 );
 
 workbox.clientsClaim();
