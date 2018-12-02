2d001577de43fc9560d3290d7db4762d6e0e1112
Conlan Rios <conlan@gmail.com>
2018-12-02 04:45:08 -0800

    Updates

--- precache-manifest.b18ccd10ef11fa911c53b9c6d03080b0.js
+++ precache-manifest.b18ccd10ef11fa911c53b9c6d03080b0.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "de8bfb8d114ea0174c17",
-    "url": "/static/js/main.de8bfb8d.chunk.js"
+    "revision": "6f16163b6e4129a3b24c",
+    "url": "/static/js/main.6f16163b.chunk.js"
   },
   {
     "revision": "e4ea8ad36f00281cf479",
     "url": "/static/js/1.e4ea8ad3.chunk.js"
   },
   {
-    "revision": "de8bfb8d114ea0174c17",
+    "revision": "6f16163b6e4129a3b24c",
     "url": "/static/css/main.c51cdafa.chunk.css"
   },
   {
-    "revision": "02fe907f69c6ce07e6f3283578b71819",
+    "revision": "05be515e1fb4e62c7e7338dfb91bbda8",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3b471f4a624700effdeb1f1369973168.js"
+  "/precache-manifest.b18ccd10ef11fa911c53b9c6d03080b0.js"
 );
 
 workbox.clientsClaim();
