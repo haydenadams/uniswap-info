2d7bc24be49ca5243dda4fbc37755ae8e9aaac23
Conlan Rios <conlan@gmail.com>
2018-12-12 04:12:41 -0800

    Updates

---
+++ precache-manifest.091af803b598a172f3cb007f17bc9b53.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "477d7616cad52d8bc0b1",
+    "url": "/static/js/4.477d7616.chunk.js"
+  },
+  {
+    "revision": "bddf17439f5d4aa48ab5",
+    "url": "/static/css/main.e5136edf.chunk.css"
+  },
+  {
+    "revision": "e53688224d770eb485ab",
+    "url": "/static/js/runtime~main.e5368822.js"
+  },
+  {
+    "revision": "2ab90e560cff7dd790e7",
+    "url": "/static/js/1.2ab90e56.chunk.js"
+  },
+  {
+    "revision": "6c8375ab88e617d0ddcc",
+    "url": "/static/js/7.6c8375ab.chunk.js"
+  },
+  {
+    "revision": "8d5e287808ef9c1d905b",
+    "url": "/static/js/2.8d5e2878.chunk.js"
+  },
+  {
+    "revision": "635703e27c57757a2d21",
+    "url": "/static/js/6.635703e2.chunk.js"
+  },
+  {
+    "revision": "69f47e924edff8a76aa3",
+    "url": "/static/js/3.69f47e92.chunk.js"
+  },
+  {
+    "revision": "c8554bd83db153940319",
+    "url": "/static/js/5.c8554bd8.chunk.js"
+  },
+  {
+    "revision": "bddf17439f5d4aa48ab5",
+    "url": "/static/js/main.bddf1743.chunk.js"
+  },
+  {
+    "revision": "6c8375ab88e617d0ddcc",
+    "url": "/static/css/7.32d58103.chunk.css"
+  },
+  {
+    "revision": "635703e27c57757a2d21",
+    "url": "/static/css/6.46b688c4.chunk.css"
+  },
+  {
+    "revision": "c8554bd83db153940319",
+    "url": "/static/css/5.46b688c4.chunk.css"
+  },
+  {
+    "revision": "477d7616cad52d8bc0b1",
+    "url": "/static/css/4.46b688c4.chunk.css"
+  },
+  {
+    "revision": "69f47e924edff8a76aa3",
+    "url": "/static/css/3.46b688c4.chunk.css"
+  },
+  {
+    "revision": "8d5e287808ef9c1d905b",
+    "url": "/static/css/2.46b688c4.chunk.css"
+  },
+  {
+    "revision": "2ab90e560cff7dd790e7",
+    "url": "/static/css/1.46b688c4.chunk.css"
+  },
+  {
+    "revision": "4c77c4e1cd0d4ca2998400bf7b10399b",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0440b5a024e2597853895aa76db60061.js"
+  "/precache-manifest.091af803b598a172f3cb007f17bc9b53.js"
 );
 
 workbox.clientsClaim();
